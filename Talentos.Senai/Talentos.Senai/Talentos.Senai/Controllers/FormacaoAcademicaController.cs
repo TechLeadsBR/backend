275d97d1a4b4516b657078080c344a62e2e03627
EricaSobral <erica.sobral95@gmail.com>
2020-09-14 12:10:43 -0300

    Enviando CRUD FormacaoAcademica

---
+++ Talentos.Senai/Talentos.Senai/Talentos.Senai/Controllers/FormacaoAcademicaController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talentos.Senai.Domains;
+using Talentos.Senai.Interfaces;
+using Talentos.Senai.Repositories;
+
+namespace Talentos.Senai.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormacaoAcademicaController : ControllerBase
+    {
+        private IFormacaoAcademica _formacaoAcademicaRepository;
+
+        public FormacaoAcademicaController()
+        {
+            _formacaoAcademicaRepository = new FormacaoAcademicaRepository();
+
+        }
+
+        [HttpGet]
+        public IActionResult Get() => Ok(_formacaoAcademicaRepository.Listar());
+
+        [HttpPost]
+        public IActionResult Post(FormacaoAcademica data)
+        {
+            TypeMessage returnRepository = _formacaoAcademicaRepository.Cadastrar(data);
+            if (returnRepository.ok) return StatusCode(201, returnRepository);
+            else return BadRequest(returnRepository);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, FormacaoAcademica data)
+        {
+            TypeMessage returnRepository = _formacaoAcademicaRepository.Atualizar(id, data);
+            if (returnRepository.ok) return Ok(returnRepository);
+            else return BadRequest(returnRepository);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            TypeMessage returnRepository = _formacaoAcademicaRepository.Deletar(id);
+            if (returnRepository.ok) return Ok(returnRepository);
+            else return BadRequest(returnRepository);
+        }
+
+    }
+
+}
---
+++ Talentos.Senai/Talentos.Senai/Talentos.Senai/Repositories/FormacaoAcademicaRepository.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Talentos.Senai.Domains;
+using Talentos.Senai.Interfaces;
+using Talentos.Senai.Utilities;
+
+namespace Talentos.Senai.Repositories
+{
+    public class FormacaoAcademicaRepository : IFormacaoAcademica
+    {
+        private TalentosContext ctx = new TalentosContext();
+        private readonly Functions _functions = new Functions();
+        private readonly string table = "formacaoAcademica";
+  
+        public List<FormacaoAcademica> Listar() => ctx.FormacaoAcademica.Include(f => f.IdAlunoNavigation).ToList();
+
+        public FormacaoAcademica BuscarPorId(int id) => ctx.FormacaoAcademica.FirstOrDefault(f => f.IdFormacaoAcademica == id);
+
+        public TypeMessage Cadastrar(FormacaoAcademica data)
+        {
+            FormacaoAcademica formacaoExistente = ctx.FormacaoAcademica.FirstOrDefault(f => f.IdFormacaoAcademica == data.IdFormacaoAcademica);
+
+            if (formacaoExistente == null)
+            {
+                try
+                {
+                    ctx.FormacaoAcademica.Add(data);
+                    ctx.SaveChanges();
+                    string okMessage = _functions.defaultMessage(table, "ok");
+                    return _functions.replyObject(okMessage, true);
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error);
+                    string errorMessage = _functions.defaultMessage(table, "error");
+                    return _functions.replyObject(errorMessage, false);
+                }
+            }
+            else
+            {
+                string existsMessage = _functions.defaultMessage(table, "exists");
+                return _functions.replyObject(existsMessage, false);
+            }
+        }
+
+        public TypeMessage Atualizar(int id, FormacaoAcademica dataFormacao)
+        {
+            FormacaoAcademica formacaoParaAtualizar = BuscarPorId(id);
+
+            if (formacaoParaAtualizar != null)
+            {
+                try
+                {
+                    formacaoParaAtualizar.NomeCurso = dataFormacao.NomeCurso ?? formacaoParaAtualizar.NomeCurso ;
+                    formacaoParaAtualizar.Instituicao = dataFormacao.Instituicao ?? formacaoParaAtualizar.Instituicao;
+                    formacaoParaAtualizar.TipoCurso = dataFormacao.TipoCurso ?? formacaoParaAtualizar.TipoCurso;
+                    formacaoParaAtualizar.InicioCurso = dataFormacao.InicioCurso != null ? dataFormacao.InicioCurso : formacaoParaAtualizar.InicioCurso;
+                    formacaoParaAtualizar.TerminoCurso = dataFormacao.TerminoCurso != null ? dataFormacao.TerminoCurso : formacaoParaAtualizar.TerminoCurso;
+                    formacaoParaAtualizar.IdAluno = dataFormacao.IdAluno ?? formacaoParaAtualizar.IdAluno;
+                    
+                    ctx.FormacaoAcademica.Update(formacaoParaAtualizar);
+                    ctx.SaveChanges();
+
+                    string okMessage = _functions.defaultMessage(table, "ok");
+                    return _functions.replyObject(okMessage, true);
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error);
+                    string errorMessage = _functions.defaultMessage(table, "error");
+                    return _functions.replyObject(errorMessage, false);
+                }
+
+            }
+            else
+            {
+                string notFoundMessage = _functions.defaultMessage(table, "notfound");
+                return _functions.replyObject(notFoundMessage, false);
+            }
+        }
+
+        public TypeMessage Deletar(int id)
+        {
+            FormacaoAcademica formacaoParaDeletar = BuscarPorId(id);
+
+            if (formacaoParaDeletar != null)
+            {
+                try
+                {
+                    ctx.FormacaoAcademica.Remove(formacaoParaDeletar);
+                    ctx.SaveChanges();
+
+                    string okMessage = _functions.defaultMessage(table, "ok");
+                    return _functions.replyObject(okMessage, true);
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error);
+                    string errorMessage = _functions.defaultMessage(table, "error");
+                    return _functions.replyObject(errorMessage, false);
+                }
+            }
+            else
+            {
+                string notFoundMessage = _functions.defaultMessage(table, "notfound");
+                return _functions.replyObject(notFoundMessage, false);
+            }
+        }
+    }
+}
