648ce22810f13dd44f93a3f51b667ef839ca0252
CarlosMorette <carlos.morette@outlook.com>
2020-10-29 08:40:43 -0230

    Upload de imagem

--- Talentos.Senai/Talentos.Senai/Controllers/UploadController.cs
+++ Talentos.Senai/Talentos.Senai/Controllers/UploadController.cs
@@ -6,19 +6,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Hosting.Internal;
+using Talentos.Senai.Repositories;
 
 namespace Talentos.Senai.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class UploadController : ControllerBase
     {
-        private readonly HostingEnvironment _hostingEnvironment;
+        private UploadImagesRepository _uploadImagesRepository;
 
 
         public UploadController(HostingEnvironment hostingEnvironment)
         {
-            _hostingEnvironment = hostingEnvironment;
+            _uploadImagesRepository = new UploadImagesRepository();
         }
 
         [HttpPost]
@@ -26,28 +27,42 @@ namespace Talentos.Senai.Controllers
         {
             // Save image
 
-            string wwwroothPath = _hostingEnvironment.WebRootPath;
-
-            var files = HttpContext.Request.Form.Files;
-
-            if (files.Count != 0)
+            try
             {
-                var ImagePath = @"Images\";
-                var Extension = Path.GetExtension(files[0].FileName);
-                Console.WriteLine(Extension);
-                var RelativeImagePath = ImagePath + files[0].FileName;
-                var AbsImagePath = Path.Combine("rootpah", RelativeImagePath);
-
-                // upload
-                using (var filetream = new FileStream(RelativeImagePath, FileMode.Create))
-                {
-                    files[0].CopyTo(filetream);
-                }
+                var file = Request.Form.Files[0];
+                _uploadImagesRepository.Upload(file, "StudentImages");
 
                 return Ok("Deu bom");
             }
+            catch (FileLoadException error)
+            {
+                Console.WriteLine("COMENTARIO DEU RUIM");
+                Console.WriteLine(error);
+                return BadRequest("Deu ruim");
+            }
 
-            return BadRequest("Não deu bom");
+
+            //var files = HttpContext.Request.Form.Files;
+
+            //if (files.Count != 0)
+            //{
+            //    var ImagePath = @"Images\";
+            //    var Extension = Path.GetExtension(files[0].FileName);
+            //    var RelativeImagePath = ImagePath + files[0].FileName;
+            //    var AbsImagePath = Path.Combine("rootpah", RelativeImagePath);
+
+               
+
+            //    // upload
+            //    using (var filetream = new FileStream(RelativeImagePath, FileMode.Create))
+            //    {
+            //        files[0].CopyTo(filetream);
+            //    }
+
+            //    return Ok("Deu bom");
+            //}
+
+            //return BadRequest("Não deu bom");
         }
 
     }
---
+++ Talentos.Senai/Talentos.Senai/Repositories/UploadImagesRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Net.Http.Headers;
+
+namespace Talentos.Senai.Repositories
+{
+    public class UploadImagesRepository
+    {
+            
+        public TypeMessage SaveImage(IFormFile arquivo, string savingFolder)
+        {
+            var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), savingFolder);
+
+            if (arquivo.Length > 0)
+            {
+                var fileName = ContentDispositionHeaderValue.Parse(arquivo.ContentDisposition).FileName.Trim('"');
+                var fullPath = Path.Combine(pathToSave, fileName);
+
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    arquivo.CopyTo(stream);
+                }
+
+                string okMessage = null;
+
+                return savingFolder + "/" + fileName;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
